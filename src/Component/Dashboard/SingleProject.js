d7a1a4f32967d53061e08af76fb8d063627c1bb9
MannanAnxari <mannananxari@gmail.com>
2023-01-03 16:10:37 +0500

    .

--- src/Component/Dashboard/SingleProject.js
+++ src/Component/Dashboard/SingleProject.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import { AppContext } from '../../context/AppContext';
 import { useParams, Link } from 'react-router-dom';
 import { Error } from '../Error';
@@ -16,12 +16,30 @@ export const SingleProject = () => {
     const params = useParams();
     const singleProject = params.singleProject;
     const [data, setData] = useState([]);
+    const [reviewdata, setReviewData] = useState([]);
 
     const [Invoice, setInvoice] = useState([]);
     const [isInvoice, setIsInvoice] = useState(true);
 
 
     useEffect(() => {
+
+
+        fetch(`${URL}api/feedback-get?project_id=${singleProject}`)
+            .then(res => res.json())
+            .then(json => {
+                if (json.success != false) {
+                    console.log(json);
+                    setReviewData([...reviewdata, json])
+                } else {
+                    toast.error("Something went wrong!");
+                }
+                setIsLoading(false);
+            }).catch((err) => {
+                setIsLoading(false);
+                toast.error("Something went wrong!");
+            });
+
         fetch(`${URL}api/single-project/${singleProject}?token=${user.data.user_token}`)
             .then((response) => response.json())
             .then((actualData) => { setData(actualData.data); setIsLoading(false); })
@@ -45,13 +63,42 @@ export const SingleProject = () => {
     }, []);
 
 
+    const review = useRef(null);
 
     const [rating, setRating] = useState(3);
 
     const handleChange = (value) => {
         setRating(value);
     }
-
+    console.log(reviewdata);
+    const handleReview = () => {
+        fetch(`${URL}api/feedback-post`,
+            {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ user_id: user.data.id, project_id: singleProject, rating: rating, review: review.current.value })
+            }
+        )
+            .then(res => res.json())
+            .then(json => {
+                if (json.success != false) {
+                    if (json) {
+                        toast.success("Review Submitted! 😊");
+                        setReviewData([...reviewdata, json])
+                    }
+                    else {
+                    }
+                } else {
+                    toast.error("Something went wrong!");
+                }
+                setIsLoading(false);
+            }).catch((err) => {
+                setIsLoading(false);
+                toast.error("Something went wrong!");
+            });
+    }
 
 
     return (
@@ -145,34 +192,46 @@ export const SingleProject = () => {
                     {data.status === "2" &&
                         <div className="row w-100 mx-auto review pe-md-4">
                             <div className="col-12 pb-3">
-                                <div className="review-box d-flex justify-content-between align-items-center">
-                                    <p className="para-sm mb-0 w-75">
-                                        Hello, world
-                                    </p>
-
-                                    <span className={"star d-flex justify-content-center align-items-center"}
-                                        style={{ color: "#fff", width: 24, height: 24, fontSize: 24 }}
-                                    >🟊</span>
-                                </div>
+                                {reviewdata && reviewdata.flat().map((item) => {
+                                    return <div className="review-box d-flex justify-content-between align-items-center my-2">
+                                        <p className="para-sm mb-0 w-75">
+                                            {item.review}
+                                        </p>
+                                        <div className="starzz d-flex">
+                                            {(() => {
+                                                let rows = [];
+                                                for (let i = 0; i < item.rating; i++) {
+                                                    rows.push(i);
+                                                }
+                                                return rows.map((item) => {
+                                                    return <span key={item} className={"star d-flex justify-content-center align-items-center"} style={{ color: "#fff", width: 24, height: 24, fontSize: 24 }} >🟊</span>
+                                                })
+                                            })()}
+                                        </div>
+                                    </div>
+                                })}
                             </div>
                             <div className="col-12">
-                                <div class="wrapper mx-auto pt-3">
-                                    <form action="#">
+                                {!reviewdata.flat().length
+                                    &&
+                                    <div class="wrapper mx-auto pt-3">
+                                        <div >
 
-                                        <StarRating
-                                            count={5}
-                                            size={40}
-                                            value={rating}
-                                            activeColor={'var(--primary)'}
-                                            inactiveColor={'#ddd'}
-                                            onChange={handleChange} />
-                                        <textarea name="opinion" cols="30" rows="5" placeholder="Your opinion..."></textarea>
-                                        <div class="btn-group">
-                                            <button type="submit" class="btn submit">Submit</button>
-                                            <button class="btn cancel">Cancel</button>
+                                            <StarRating
+                                                count={5}
+                                                size={40}
+                                                value={rating}
+                                                activeColor={'var(--primary)'}
+                                                inactiveColor={'#ddd'}
+                                                onChange={handleChange} />
+                                            <textarea id='revieww' name="opinion" cols="30" rows="5" placeholder="Your review..." ref={review}></textarea>
+                                            <div class="btn-group">
+                                                <button type="submit" class="btn submit" onClick={handleReview}>Submit</button>
+                                                <button class="btn cancel">Cancel</button>
+                                            </div>
                                         </div>
-                                    </form>
-                                </div>
+                                    </div>
+                                }
                             </div>
                         </div>
 
